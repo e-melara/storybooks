b55cdf77d369d0ccc8a9ba470a9899f9ebd52f19
Edwin Melara <edwin.melara.landaverde@gmail.com>
2022-02-12 17:26:14 -0600

    Comenzado con storybook

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,49 @@
+import "./MyLabel.css";
+
+/**
+ * Is this the principal call to action on the page?
+ */
+
+interface MyLabelProps {
+  /**
+   * Este es el mensaje en el label
+   */
+  label: string;
+  /**
+   * Este es el tamaño del label (Default: normal)
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Esta propieda define si sera en mayuscula la palabra
+   */
+  allCaps: boolean;
+  /**
+   * El color del texto
+   */
+  color: "primary" | "secondary" | "tertiary";
+  /**
+   * Selecciona el color del texto
+   */
+  fontColor?: "string";
+}
+
+export const MyLabel = ({
+  label = "No Label",
+  size = "normal",
+  allCaps = false,
+  color = "primary",
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <>
+      <span
+        className={["label", size, "text-" + color].join(" ")}
+        style={{
+          color: fontColor,
+        }}
+      >
+        {allCaps ? label.toUpperCase() : label}
+      </span>
+    </>
+  );
+};
